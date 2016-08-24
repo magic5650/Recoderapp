5a1dad6066416cb26724b79948e6845db203a91c
magic5650 <magic5650@126.com>
2016-08-24 18:31:00 +0800

    判断开始暂停

--- app/src/main/java/com/magicrecoder/recoderapp/RecorderActivity.java
+++ app/src/main/java/com/magicrecoder/recoderapp/RecorderActivity.java
@@ -1,82 +1,77 @@
 package com.magicrecoder.recoderapp;
 
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.os.Build;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 
 public class RecorderActivity extends AppCompatActivity {
 
     private static final String TAG = "Lifecycle";
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
+    //定义数据
+    private List<Recorder> mData;
+    //定义ListView对象
+    private ListView mListViewArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recorder);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
             localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
         }
+        setContentView(R.layout.activity_recorder);
+        //为ListView对象赋值
+        mListViewArray = (ListView) findViewById(R.id.recorder_ListView);
+        LayoutInflater inflater = getLayoutInflater();
+        //初始化数据
+        initData();
+        //创建自定义Adapter的对象
+        MyAdapter adapter =new MyAdapter(inflater,mData);
+        //将布局添加到ListView中
+        mListViewArray.setAdapter(adapter);
+        //添加Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //Toolbar监听
         toolbar.setOnMenuItemClickListener(onMenuItemClick);
-
-        if(getSupportActionBar() != null)
-            // Enable the Up button
-        {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
-
         Log.d(TAG, "MainActivity onCreate 创建，执行");
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+    /*
+    初始化数据
+     */
+    private void initData() {
+        mData = new ArrayList <>();
+        Recorder recorder_one  = new Recorder("20160820", "30s", "录音1", "8月20号",R.mipmap.appicon );
+        Recorder recorder_two  = new Recorder("20160720", "01:30", "录音2", "7月20号",R.mipmap.appicon );
+        Recorder recorder_three  = new Recorder("20160620", "11:01", "录音3", "6月20号",R.mipmap.appicon );
+        mData.add(recorder_one);
+        mData.add(recorder_two);
+        mData.add(recorder_three);
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
         Log.d(TAG, "MainActivity onStart 可见 执行");
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Recorder Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.magicrecoder.recoderapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
     @Override
@@ -84,28 +79,46 @@ public class RecorderActivity extends AppCompatActivity {
         super.onResume();
         Log.d(TAG, "MainActivity onResume 获取焦点 执行");
 
-
-        ImageView imageView = (ImageView) findViewById(R.id.begin_record);
-        imageView.setImageResource(R.mipmap.ic_toggle_radio_button_on);
-        imageView.setOnClickListener(new ImageView.OnClickListener() {
-            private String choose="begin";
-            ImageView begin_record = (ImageView) findViewById(R.id.begin_record);
+        final ImageView begin_record = (ImageView) findViewById(R.id.begin_record);
+        final ImageView save_record = (ImageView) findViewById(R.id.save_record);
+        assert begin_record != null;
+        assert save_record != null;
+        begin_record.setOnClickListener(new ImageView.OnClickListener() {;
             @Override
             public void onClick(View arg0) {
-                if (choose=="begin") {
-                    begin_record.setImageResource(R.mipmap.ic_av_pause_circle_fill);
-                    choose="pause";
-                    Log.d(TAG, "点击开始录音");
+                Integer  integer  = (Integer ) begin_record.getTag();
+                integer = integer == null ? 0 : integer;
+                switch (integer){
+                    case R.mipmap.ic_toggle_radio_button_on:
+                    default:
+                        begin_record.setImageResource(R.mipmap.ic_av_pause_circle_fill);
+                        begin_record.setTag(R.mipmap.ic_av_pause_circle_fill);
+                        save_record.setImageResource(0);
+                        Log.d(TAG,"开始录音" + integer);
+                        break;
+                    case R.mipmap.ic_av_pause_circle_fill:
+                        begin_record.setImageResource(R.mipmap.ic_toggle_radio_button_on);
+                        begin_record.setTag(R.mipmap.ic_toggle_radio_button_on);
+                        save_record.setImageResource(R.drawable.ic_save_nactive_24dp);
+                        Log.d(TAG,"暂停录音" + integer);
+                        break;
                 }
-                else if(choose=="pause") {
+            }
+        });
+        save_record.setOnClickListener(new ImageView.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if(save_record.getDrawable() == null){
+                    Log.d(TAG, "保存图片为空");
+                }
+                else {
+                    Log.d(TAG, "点击保存录音");
                     begin_record.setImageResource(R.mipmap.ic_toggle_radio_button_on);
-                    choose="begin";
-                    Log.d(TAG, "点击暂停录音");
+                    save_record.setImageResource(0);
                 }
             }
         });
     }
-
     @Override
     public void onPause() {
         super.onPause();
@@ -115,23 +128,8 @@ public class RecorderActivity extends AppCompatActivity {
     @Override
     public void onStop() {
         super.onStop();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Recorder Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.magicrecoder.recoderapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
         Log.d(TAG, "MainActivity onStop 不可见 执行");
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.disconnect();
+
     }
 
     @Override
